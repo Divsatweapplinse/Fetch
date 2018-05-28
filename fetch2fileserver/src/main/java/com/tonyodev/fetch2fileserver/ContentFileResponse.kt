d20af25fecaf3c2a076e65b33f5a07ec26bc12c2
Tonyo Francis <tonyo.dev@gmail.com>
2018-05-28 15:35:34 -0400

    transporter fixes

--- fetch2fileserver/src/main/java/com/tonyodev/fetch2fileserver/ContentFileResponse.kt
+++ fetch2fileserver/src/main/java/com/tonyodev/fetch2fileserver/ContentFileResponse.kt
@@ -16,11 +16,11 @@ data class ContentFileResponse(val status: Int = HttpURLConnection.HTTP_UNSUPPOR
             val builder = StringBuilder()
                     .append('{')
                     .append("\"Status\":").append(status).append(',')
-                    .append("\"Type\":").append(type).append(',')
+                    .append("\"Md5\":").append("\"$md5\"").append(',')
                     .append("\"Connection\":").append(connection).append(',')
                     .append("\"Date\":").append(date).append(',')
                     .append("\"ContentLength\":").append(contentLength).append(',')
-                    .append("\"Md5\":").append("\"$md5\"")
+                    .append("\"Type\":").append(type)
                     .append('}')
             return builder.toString()
         }
